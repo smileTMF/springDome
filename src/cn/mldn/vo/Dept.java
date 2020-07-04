bb49365884ccf8d62cebcd634fb3f5d128ca441d
smileTMF <1912961423@qq.com>
2020-07-04 15:56:41 +0800

    first commit

---
+++ src/cn/mldn/vo/Dept.java
@@ -0,0 +1,25 @@
+package cn.mldn.vo;
+
+public class Dept {
+    private String dname;
+    private String dno;
+
+    public Dept() {
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getDno() {
+        return dno;
+    }
+
+    public void setDno(String dno) {
+        this.dno = dno;
+    }
+}
---
+++ src/cn/mldn/vo/Emp.java
@@ -0,0 +1,33 @@
+package cn.mldn.vo;
+
+public class Emp {
+    private String name ;
+    private Integer age;
+    private Dept dept;
+
+    public Emp() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Dept getDept() {
+        return dept;
+    }
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+}
